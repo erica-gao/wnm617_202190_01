566d1f65e5cec08e3c1e8dd21c229bc0599774a6
Erica Gao <erica.gao1103@gmail.com>
2021-10-21 08:56:11 +0800

    updated midterm project

--- gao.erica/js/app.js
+++ gao.erica/js/app.js
@@ -16,7 +16,7 @@ $(()=>{
    .on("submit","#list-add-form",function(e) {
       e.preventDefault();
    })
- 
+
 
    // ANCHOR CLICKS
    .on("click",".js-logout",function(e) {
@@ -28,21 +28,35 @@ $(()=>{
 
 
    .on("click","[data-activate]",function(e){
-      e.preventDefault();
       let target = $(this).data("activate");
       $(target).addClass("active");
    })
    .on("click","[data-deactivate]",function(e){
-      e.preventDefault();
       let target = $(this).data("deactivate");
       $(target).removeClass("active");
    })
    .on("click","[data-toggle]",function(e){
-      e.preventDefault();
       let target = $(this).data("toggle");
       $(target).toggleClass("active");
    })
+   .on("click","[data-activateone]",function(e){
+      let target = $(this).data("activateone");
+      console.log(target)
+      $(target).addClass("active").siblings().removeClass('active');
+   })
+   ;
 
+
+   $("[data-template]").each(function(){
+      let target = $(this).data("template");
+      $(this).html($(target).html());
+   })
+
+   $({
+      "#page-map":".nav-icon-set li:nth-child(1)",
+      "#page-list":".nav-icon-set li:nth-child(2)",
+      "#page-user-profile":".nav-icon-set li:nth-child(3)",
+   }[location.hash]).addClass("active");
 
 
 });
